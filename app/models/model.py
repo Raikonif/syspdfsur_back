fb7b75d889936ec3e9c3883864eafe69d3e98e14
Raikonif <raikonif@gmail.com>
2023-05-09 08:26:54 -0400

    model table implemented and env file

--- app/models/model.py
+++ app/models/model.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean
 from app.config import Base
 
 
-class Book(Base):
+class Patient(Base):
     __tablename__ = "patient"
     id = Column(Integer, primary_key=True)
     name = Column(String)
