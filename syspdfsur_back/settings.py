7b50fd26962b6fa79280f1a3fde23932016b1e01
Raikonif <raikonif@gmail.com>
2023-07-02 11:21:45 -0400

    deleted the api and implemented the api app to use DRF

--- syspdfsur_back/settings.py
+++ syspdfsur_back/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "corsheaders",
-    "admin_app",
-    "posts_app",
+    # "admin_app",
+    # "posts_app",
 ]
 
 MIDDLEWARE = [
--- syspdfsur_back/urls.py
+++ syspdfsur_back/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("admin_app/", include("admin_app.urls")),
-    path("posts_app/", include("posts_app.urls")),
+    # path("admin_app/", include("admin_app.urls")),
+    # path("posts_app/", include("posts_app.urls")),
 ]
