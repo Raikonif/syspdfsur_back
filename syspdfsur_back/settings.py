d3ad1ef62f5fedf7052019d1bc8f9804208054da
Raikonif <raikonif@gmail.com>
2024-01-31 21:04:15 -0400

    added a library binary

--- syspdfsur_back/settings.py
+++ syspdfsur_back/settings.py
@@ -83,6 +83,7 @@ DATABASES = {
     }
 }
 
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- syspdfsur_back/urls.py
+++ syspdfsur_back/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from api import views
-
+from django.contrib.staticfiles import staticfiles_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # path("api/", include("api.urls")),
 ]
+urlpatterns += staticfiles_urlpatterns()
