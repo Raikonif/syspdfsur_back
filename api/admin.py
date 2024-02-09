603b290af10d4c3c150657258d5e0e330e6e2792
Raikonif <raikonif@gmail.com>
2024-02-08 21:29:09 -0400

    corrected models and admin problem to seem in the django control panel

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
-from .models import User, Author, Article, Image
-# Register your models here.
-
-admin.site.register(User)
-admin.site.register(Author)
-admin.site.register(Article)
-admin.site.register(Image)
+# from django.contrib import admin
+# from .models import User, Author, Article, Image
+# # Register your models here.
+#
+# admin.site.register(User)
+# admin.site.register(Author)
+# admin.site.register(Article)
+# admin.site.register(Image)
