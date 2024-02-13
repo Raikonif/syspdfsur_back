4e545698d51e9023171746cfffe27cf1ee59394d
Raikonif <raikonif@gmail.com>
2024-02-13 17:26:06 -0400

    datetimefield modified and can access to articles endpoint

--- api/models.py
+++ api/models.py
@@ -7,6 +7,9 @@ class User(models.Model):
     role = models.CharField(max_length=15)
     email = models.EmailField().unique
 
+    def __str__(self):
+        return self.username + " " + self.email + " " + self.role
+
     class Meta:
         db_table = "users"
 
@@ -19,6 +22,9 @@ class Author(models.Model):
     phone = models.CharField(max_length=20)
     facebook = models.CharField(max_length=40)
 
+    def __str__(self):
+        return self.mention + " " + self.fullname
+
     class Meta:
         db_table = "authors"
 
@@ -28,7 +34,10 @@ class Article(models.Model):
     content = models.TextField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     type = models.CharField(max_length=30)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title + " / " + self.type
 
     class Meta:
         db_table = "articles"
@@ -37,6 +46,9 @@ class Article(models.Model):
 class Image(models.Model):
     url = models.CharField(max_length=255)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.url
 
     class Meta:
         db_table = 'images'
